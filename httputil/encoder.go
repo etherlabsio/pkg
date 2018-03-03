e2ed569fd181438886b8847d798eb41673d315e1
Karthik Muralidharan <mkarthik91@gmail.com>
2018-03-03 23:49:58 +0530

    add; httputil package

---
+++ httputil/encoder.go
@@ -0,0 +1,26 @@
+package httputil
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	kithttp "github.com/go-kit/kit/transport/http"
+	"gitlab.com/etherlabs/pkg/errors"
+)
+
+var errYouAreDoingItWrong = errors.New("programmer error")
+
+// DefaultErrorEncoder takes in a status coder and returns an HTTP error encoder
+func DefaultErrorEncoder(statusCoder func(err error) int) kithttp.ErrorEncoder {
+	return func(_ context.Context, err error, w http.ResponseWriter) {
+		if err == nil {
+			err = errors.WithMessage(errYouAreDoingItWrong, "encodeError received nil error")
+		}
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(statusCoder(err))
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"error": err.Error(),
+		})
+	}
+}
