604aa920473870f6059b35b27aecfb89551037da
Karthik Muralidharan <mkarthik91@gmail.com>
2018-09-13 22:52:10 +0530

    add; absFilePath

--- fs/filesystem.go
+++ fs/filesystem.go
@@ -4,12 +4,14 @@ package fs
 import (
 	"archive/tar"
 	"compress/gzip"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
+	"strings"
 
+	"github.com/etherlabsio/errors"
 	"github.com/etherlabsio/pkg/env"
-	"github.com/etherlabsio/pkg/errors"
 )
 
 const (
@@ -33,26 +35,26 @@ func CopyDir(src, dest string) error {
 	const op = errors.Op("CopyDir")
 	dir, err := os.Open(src)
 	if err != nil {
-		return errors.New(err, errors.IO, op)
+		return errors.WithOp(err, op)
 	}
 	if err := os.MkdirAll(dest, DirMode); err != nil {
-		return errors.New(err, errors.IO, op)
+		return errors.WithOp(err, op)
 	}
 
 	files, err := dir.Readdir(-1)
 	if err != nil {
-		return errors.New(err, errors.IO, op)
+		return errors.WithOp(err, op)
 	}
 	for _, file := range files {
 		srcptr := filepath.Join(src, file.Name())
 		dstptr := filepath.Join(dest, file.Name())
 		if file.IsDir() {
 			if err := CopyDir(srcptr, dstptr); err != nil {
-				return errors.New(err, errors.IO, op)
+				return errors.WithOp(err, op)
 			}
 		} else {
 			if err := CopyFile(srcptr, dstptr); err != nil {
-				return errors.New(err, errors.IO, op)
+				return errors.WithOp(err, op)
 			}
 		}
 	}
@@ -65,23 +67,23 @@ func CopyFile(src, dest string) error {
 	const op = errors.Op("CopyFile")
 	source, err := os.Open(src)
 	if err != nil {
-		return errors.New(op, errors.IO, err)
+		return errors.WithOp(err, op)
 	}
 	defer source.Close()
 
 	destfile, err := os.Create(dest)
 	if err != nil {
-		return errors.New(op, errors.IO, err)
+		return errors.WithOp(err, op)
 	}
 	defer destfile.Close()
 
 	_, err = io.Copy(destfile, source)
 	if err != nil {
-		return errors.New(op, errors.IO, err)
+		return errors.WithOp(err, op)
 	}
 	sourceinfo, err := os.Stat(src)
 	if err != nil {
-		return errors.New(op, errors.IO, err)
+		return errors.WithOp(err, op)
 	}
 
 	return os.Chmod(dest, sourceinfo.Mode())
@@ -92,14 +94,13 @@ func UntarBundle(destination string, source string) error {
 	const op = errors.Op("UntarBundle")
 	f, err := os.Open(source)
 	if err != nil {
-		return errors.New(err, op, errors.IO, "open download source")
+		return errors.New("open download source", err, op, errors.IO)
 	}
 	defer f.Close()
 
 	gzr, err := gzip.NewReader(f)
 	if err != nil {
-		err = errors.New(errors.IO, op, err)
-		return errors.Wrapf(err, "create gzip reader from %s", source)
+		return errors.New("create gzip reader from "+source, errors.IO, err, op)
 	}
 	defer gzr.Close()
 
@@ -110,29 +111,42 @@ func UntarBundle(destination string, source string) error {
 			break
 		}
 		if err != nil {
-			return errors.New(err, op, errors.IO, "reading tar file")
+			return errors.New("reading tar file", err, op, errors.IO)
 		}
 
 		path := filepath.Join(filepath.Dir(destination), header.Name)
 		info := header.FileInfo()
 		if info.IsDir() {
 			if err = os.MkdirAll(path, info.Mode()); err != nil {
-				err = errors.New(errors.IO, op, err)
-				return errors.Wrapf(err, "creating directory for tar file: %s", path)
+				return errors.New("creating directory for tar file: "+path, errors.IO, op, err)
 			}
 			continue
 		}
 
 		file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, info.Mode())
 		if err != nil {
-			errors.New(err, op, errors.IO)
-			return errors.Wrapf(err, "open file %s", path)
+			return errors.New("open file "+path, err, op, errors.IO)
 		}
 		defer file.Close()
 		if _, err := io.Copy(file, tr); err != nil {
-			errors.New(err, op, errors.IO)
-			return errors.Wrapf(err, "copy tar %s to destination %s", header.FileInfo().Name(), path)
+			msg := fmt.Sprintf("copy tar %s to destination %s", header.FileInfo().Name(), path)
+			return errors.New(msg, err, op, errors.IO)
 		}
 	}
 	return nil
+}
+
+// AbsFilePath resolves ENV vars specified in string for file paths
+func AbsFilePath(inPath string) string {
+	if strings.HasPrefix(inPath, "$") {
+		end := strings.Index(inPath, string(os.PathSeparator))
+		inPath = os.Getenv(inPath[1:end]) + inPath[end:]
+	}
+	if filepath.IsAbs(inPath) {
+		return filepath.Clean(inPath)
+	}
+	if p, err := filepath.Abs(inPath); err == nil {
+		return filepath.Clean(p)
+	}
+	return ""
 }
