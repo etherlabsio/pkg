70413aa034bf741574efd3f36300eeaa1a54e0cc
Mithun BS <mithun@etherlabs.io>
2019-06-04 14:46:51 +0530

    nats client add reconnect logic and logs during disconnect/reconnect

---
+++ pubsub/nats/nats.go
@@ -0,0 +1,24 @@
+package pubsubnats
+
+import (
+	"github.com/go-kit/kit/log"
+	"github.com/nats-io/go-nats"
+	"github.com/go-kit/kit/log/level"
+)
+
+func WithDefaultConnectOptions(natsClientName string, logger log.Logger) ([]nats.Option) {
+	var options []nats.Option
+	options = append(options, nats.ReconnectHandler(func(c *nats.Conn) {
+		level.Info(logger).Log("natsclient", natsClientName, "handler", "ReconnectHandler", "url", c.ConnectedUrl())
+	}))
+	options = append(options, nats.DisconnectHandler(func(c *nats.Conn) {
+		level.Error(logger).Log("natsclient", natsClientName, "handler", "DisconnectHandler")
+	}))
+	options = append(options, nats.ClosedHandler(func(c *nats.Conn) {
+		level.Error(logger).Log("natsclient", natsClientName, "handler", "ClosedHandler")
+	}))
+	options = append(options, nats.MaxReconnects(-1))
+	options = append(options, nats.Name(natsClientName))
+
+	return options
+}
